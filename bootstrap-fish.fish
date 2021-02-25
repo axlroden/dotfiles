e655951803ead0274af958befa2585c1ab7c50f4
Alex Rodenberg <arodenberg@danmagi.com>
2021-02-25 14:37:02 +0100

    Update bootstrap-fish.fish
    simplified

--- bootstrap-fish.fish
+++ bootstrap-fish.fish
@@ -1,31 +1,8 @@
 #!/usr/local/bin/fish
-curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 
-curl https://raw.githubusercontent.com/docker/compose/1.21.2/contrib/completion/fish/docker-compose.fish > ~/.config/fish/completions/docker-compose.fish
-curl https://raw.githubusercontent.com/docker/cli/master/contrib/completion/fish/docker.fish > ~/.config/fish/completions/docker.fish
-
-fisher add docker-completion
-fisher add pipenv
-fisher add laughedelic/pisces
-fisher add laughedelic/brew-completions
-fisher add pyenv
-fisher add tab
-fisher add done
-fisher add omf/osx
-fisher add z
-fisher add grc
-fisher add homebrew-tap
-fisher add grc
-fisher add gitignore
-fisher add ghost
-fisher add edc/bass
-fisher add omf/extract
-fisher add omf/sublime
-fisher add omf/ssh
-fisher add omf/python
-fisher add omf/git-flow
-fisher add omf/django
-fisher add omf/composer
-fisher add omf/bundler
-fisher add omf/brew
-fisher add omf/theme-bobthefish
+fisher install brgmnn/fish-docker-compose
+fisher install sentriz/fish-pipenv
+fisher install laughedelic/brew-completions
+fisher install edc/bass
+fisher install jorgebucaran/hydro
